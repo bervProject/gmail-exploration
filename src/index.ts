22f5fe47968ef11faba6f3b02cc15a43c48f9a1e
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-05-17 09:31:51 +0700

    add env and publish

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import app from './app';
 import cron from 'node-cron';
 import logger from './logger';
 
-cron.schedule('0 */3 * * *', () => {
+const defaultSchedule = process.env.CRON_TIME || '0 */3 * * *';
+
+cron.schedule(defaultSchedule, () => {
     logger.info('Run spam cleaner!');
     app.start();
 });
 
+logger.info("Cron Scheduled");
